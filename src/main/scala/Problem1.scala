f7af273cf03e7d377915ddb80ddfb02fe43367e3
Deeds67 <pierrem@live.co.za>
2017-04-06 20:38:56 +0200

    Add src folder

---
+++ src/main/scala/Problem1.scala
@@ -0,0 +1,17 @@
+package main
+
+object Main extends App {
+//  If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//  Find the sum of all the multiples of 3 or 5 below 1000.
+  def sumOfMultiples(fromNumber: Int, toNumber: Int, numbers: List[Int]): Int = {
+    (fromNumber until toNumber) reduce { (acc, num) =>
+      val sum = if(numbers.exists(num % _ == 0)) num else 0
+      acc + sum
+    }
+  }
+  val answer = sumOfMultiples(0, 1000, List(3, 5))
+  println(answer)
+}
+
+
+
