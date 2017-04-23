1ed2290a47bfa4defd106afa0d53fcea10a8a95d
Deeds67 <pierrem@live.co.za>
2017-04-23 11:11:41 +0200

    Solve problem 3.

---
+++ src/main/scala/Problem3.scala
@@ -0,0 +1,23 @@
+package main
+
+import scala.annotation.tailrec
+
+object Problem3 extends App {
+//  The prime factors of 13195 are 5, 7, 13 and 29.
+//  What is the largest prime factor of the number 600851475143 ?
+  def isPrime(n: Long): Boolean = !(2 +: (3 to Math.sqrt(n).toInt by 2) exists (n%_ == 0))
+
+  def largestPrime(num: Long): Long = {
+    @tailrec
+    def increment(n: Long, currentPrime: Long = 2, largestPrime: Long = 0): Long = {
+      if (currentPrime > n) largestPrime
+      else if (n % currentPrime == 0 && isPrime(currentPrime)) {
+        increment(n / currentPrime, currentPrime + 1, currentPrime)
+      } else increment(n, currentPrime + 1, largestPrime)
+    }
+    increment(num)
+  }
+
+  println(largestPrime(600851475143l))
+
+}
