843c855300e78e4500fafbe72287cc4640ea2954
Deeds67 <pierrem@live.co.za>
2017-04-23 11:58:04 +0200

    Solve problem 4

---
+++ src/main/scala/Problem4.scala
@@ -0,0 +1,23 @@
+package main
+
+object Problem4 extends App {
+  def isPalindrome(num: Int): Boolean = {
+    val productArray = num.toString.toArray
+    val split = productArray.splitAt(productArray.length / 2)
+    split._1.sameElements(split._2.reverse)
+  }
+
+  def largestPalindromeProduct(p1: Int = 999, p2: Int = 999, largest: Int = 0): Int = {
+    val prod = p1 * p2
+    if (p1 == 100 && p2 == 100) largest
+    else if (p1 == 100) {
+      largestPalindromeProduct(p2, p2 - 1, if(prod > largest && isPalindrome(prod)) prod else largest)
+    }
+    else {
+      largestPalindromeProduct(p1 - 1, p2, if(prod > largest && isPalindrome(prod)) prod else largest)
+    }
+  }
+
+
+  println(largestPalindromeProduct())
+}
