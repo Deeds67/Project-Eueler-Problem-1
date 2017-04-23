2d3cc7dce790edb22fca8090fde638b602fe6074
Deeds67 <pierrem@live.co.za>
2017-04-23 12:07:34 +0200

    Rename functions

--- src/main/scala/Problem5.scala
+++ src/main/scala/Problem5.scala
@@ -1,14 +1,14 @@
 package main
 
 object Problem5 extends App {
-  def isDivisibleByAllNumbersTo(num: Int, end: Int = 20): Boolean = {
+  def isEvenlyDivisibleByAllNumbersInRange(num: Int, end: Int = 20): Boolean = {
     (1 to end).forall(i => num % i == 0)
   }
 
-  def findSmallest(i: Int): Int = {
-    if(isDivisibleByAllNumbersTo(i)) i
-    else findSmallest(i + 1)
+  def findSmallestEvenlyDivisible(i: Int): Int = {
+    if(isEvenlyDivisibleByAllNumbersInRange(i)) i
+    else findSmallestEvenlyDivisible(i + 1)
   }
 
-  println(findSmallest(1))
+  println(findSmallestEvenlyDivisible(1))
 }
